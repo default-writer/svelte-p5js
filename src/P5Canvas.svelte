e33b30d88b05f1582a0797e9022d926f4910bb8a
Artur Mustafin <me@experimentalcommunity.org>
2019-10-06 22:17:52 +0300

    Update P5Canvas.svelte

--- src/P5Canvas.svelte
+++ src/P5Canvas.svelte
@@ -2,8 +2,13 @@
 	export let id;
 	import p5 from "./p5.min";
 	export let sketch;
-	let myp5 = new p5(sketch, id);
-;
+
+	import { onMount } from "svelte";
+
+	onMount(function() {
+	  console.log(id);
+	  let myp5 = new p5(sketch, id);
+	});
 </script>
 
 <style>
